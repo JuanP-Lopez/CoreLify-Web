44c003723068c6de710c6aab1cb453076d7668f0
JP-Koku <137343283+JP-Koku@users.noreply.github.com>
2025-11-21 18:44:01 -0600

    correccion

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -21,7 +21,7 @@ function News() {
         body : JSON.stringify(data),
       });
 
-      const result = await response.JSON();
+      const result = await res.JSON();
       console.log("Respuesta del servidor: ", result);
 
     } catch (error) {
